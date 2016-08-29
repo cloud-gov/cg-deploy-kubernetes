d270949a4a55d1312bab54f7c88f5173a3f45bbf
Joshua Carp <jm.carp@gmail.com>
2016-08-28 22:39:49 -0400

    Add single-node redis acceptance test.

---
+++ acceptance/run-acceptance-test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+set -u
+
+function cleanup() {
+  cf delete -f ${APP_NAME}
+  cf delete-service -f ${SERVICE_INSTANCE_NAME}
+}
+
+cd ${TEST_PATH}
+
+cf login \
+  -a $CF_API_URL \
+  -u $CF_USERNAME \
+  -p $CF_PASSWORD \
+  -o $CF_ORGANIZATION \
+  -s $CF_SPACE
+
+cleanup
+
+cf create-service ${SERVICE_NAME} ${PLAN_NAME} ${SERVICE_INSTANCE_NAME}
+
+cf push --no-start -f ./manifest.yml
+cf bind-service ${APP_NAME} ${SERVICE_INSTANCE_NAME}
+cf start ${APP_NAME}
+
+cleanup
