a7e5a6f7077d0834f15d044810b6e5da6641195e
Chris Nelson <christopher.nelson@gsa.gov>
2017-07-25 12:19:49 -0700

    expose k8s and elastic nodes to tests

--- acceptance/elasticsearch24/elastic-ha.sh
+++ acceptance/elasticsearch24/elastic-ha.sh
@@ -2,4 +2,12 @@
 set -eux
 
 # show cluster health
-curl -v "https://${url}/health"
+curl -v "https://${url}/cluster-health"
+
+
+# show cluster nodes
+curl -v "https://${url}/cluster-nodes"
+
+# mess with k8s
+curl -kv -u'${K8S_USERNAME}:${K8S_PASSWORD}' ${K8S_APISERVER}
+
