03475d8ea6ce0b38062fb98c30cdf26543a6c329
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2017-08-03 14:39:00 -0400

    Initial commit for redis32 tests

---
+++ acceptance/redis32/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/cloudfoundry-community/go-cfenv"
+	"github.com/garyburd/redigo/redis"
+)
+
+func checkStatus(err error) {
+	if err != nil {
+		log.Fatalf("error: %s", err.Error())
+	}
+}
+
+func writeError(w http.ResponseWriter, err error) {
+	message, _ := json.Marshal(map[string]string{
+		"error": err.Error(),
+	})
+	w.Write(message)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusInternalServerError)
+}
+
+var client redis.Conn
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Set and check value
+	_, err := client.Do("SET", "test", "test")
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+
+	value, err := redis.String(client.Do("GET", "test"))
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+	if value != "test" {
+		writeError(w, fmt.Errorf("incorrect value: %s", value))
+		return
+	}
+
+	_, err = client.Do("DEL", "test")
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+}
+
+func main() {
+	// Get redis32-multinode credentials
+	env, _ := cfenv.Current()
+	services, _ := env.Services.WithLabel("redis32")
+	if len(services) != 1 {
+		log.Fatal("redis32 service not found")
+	}
+	creds := services[0].Credentials
+
+	// Create redis client
+	var err error
+	client, err = redis.Dial("tcp", fmt.Sprintf("%s:%s", creds["hostname"], creds["port"]))
+	checkStatus(err)
+	defer client.Close()
+
+	_, err = client.Do("AUTH", creds["password"].(string))
+	checkStatus(err)
+
+	// Serve HTTP
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), nil))
+}
