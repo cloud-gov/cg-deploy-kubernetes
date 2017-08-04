4ffde7f447ed85fad448fa5b81114f099e897e1f
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2017-08-04 15:44:13 -0400

    Add sketch for redis-ha orchestration

---
+++ acceptance/redis32/redis-ha.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+set -xue
+
+# run a few tests
+
+# destroy server/0 via k8s API
+
+sleep 5
+
+# run a few tests
+
+# destroy server/0 & server/0 via k8s API
+
+sleep 10
+
+# run a few tests
+
+# destroy all servers via k8s API
+
+sleep 20
+
+# run a few tests
+
+# Destroy a random sentinel
+
+sleep 10
+
+# run a few tests
+
+# destroy a random proxy via k8s API
+
+sleep 5
+
+# run a few tests
