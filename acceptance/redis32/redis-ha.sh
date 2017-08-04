4e13256f22a090b95399aee9100b1e2efd3ea5e5
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2017-08-04 16:27:00 -0400

    Get all pods by service id;
    This is going to be useful for the next steps. Iteration on this is a
    little slow because I need to be inside a jumpbox.

--- acceptance/redis32/redis-ha.sh
+++ acceptance/redis32/redis-ha.sh
@@ -3,6 +3,11 @@
 
 set -xue
 
+
+# get all pods by service_id
+curl -ks -u"${K8S_USERNAME}:${K8S_PASSWORD}" "${K8S_APISERVER}/api/v1/namespaces/default/pods/" | \
+jq '.items[] | select( .metadata.name | test( "'"${SERVICE_ID}"'" ) ) | { name: .metadata.name, node: .status.hostIP, ip: .status.podIP }'
+
 # run a few tests
 
 # destroy server/0 via k8s API
