534303e165c31c08507182e7f2390249be2245a8
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2017-08-09 13:08:35 -0400

    Fix get_k8s_pods

--- acceptance/redis32/redis-ha.sh
+++ acceptance/redis32/redis-ha.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -xe
+set -xue
 
 # Test Redis is working by running `SET`, `GET`, and `DEL`.
 run_tests() {
@@ -13,9 +13,9 @@ run_tests() {
 
 # Get all pods from Kubernetes matching $idx_and_short_serviceid
 get_k8s_pods() {
-  curl -ksf -u"${K8S_USERNAME}:${K8S_PASSWORD}" "${K8S_APISERVER}/api/v1/namespaces/default/pods?labelSelector=idx_and_short_serviceid%3D${idx_and_short_serviceid}" | \
+  curl -G -ksf -u"${K8S_USERNAME}:${K8S_PASSWORD}" "${K8S_APISERVER}/api/v1/namespaces/default/pods?labelSelector=idx_and_short_serviceid%3D${idx_and_short_serviceid}" | \
   jq '.items[] | { name: .metadata.name, node: .status.hostIP, ip: .status.podIP, status: .status.phase }' \ |
-  jq -c -s '.'
+  jq -s '.'
 }
 
 # Get the current primary server's IP address
@@ -64,6 +64,13 @@ primary_server_name=$(
   echo "$(get_k8s_pods)" | \
   jq '.[] | select( .ip == "'${primary_server_ip}'") | .name'
 )
+
+# Check to see if there were any errors retrieving $primary_server_name
+if ! echo $primary_server_name | grep -oE 'x[a-zA-Z0-9]{3,15}' > /dev/null
+then
+  echo "There was an error getting the primary server's name for ${primary_server_ip}"
+  exit 1
+fi
 
 # Delete the current master
 curl -ks -u"${K8S_USERNAME}:${K8S_PASSWORD}" \
