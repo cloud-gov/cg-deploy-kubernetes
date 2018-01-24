e8030c5b99cf8f23497a851fe794f2b657a6f01b
Joshua Carp <jm.carp@gmail.com>
2018-01-24 10:54:57 -0500

    [WIP] Test elasticsearch56 plan. (#111)
    * Test elasticsearch56 plan.
    
    * Test single-node elasticsearch56 plan.
    
    * Build custom images.

---
+++ acceptance/elasticsearch56/elastic-ha.sh
@@ -0,0 +1,160 @@
+#!/bin/bash
+set -eux
+
+cluster_status() {
+	curl -s https://${url}/cluster-health | jq -r .status
+}
+
+cluster_master() {
+	curl -s https://${url}/cluster-state | jq -r '.nodes[.master_node].name'
+}
+
+master_count() {
+	curl -s https://${url}/cluster-health | jq '.number_of_nodes-.number_of_data_nodes'
+}
+
+data_count() {
+	curl -s https://${url}/cluster-health | jq .number_of_data_nodes
+}
+
+check_status() {
+	counter=120
+	until [ $counter -le 0 ]; do
+		if [ $(cluster_status) == "red" ]; then
+			echo "Cluster status is red. This shouldn't happen."
+			return 1
+		fi
+		if [ $(cluster_status) != "green" ]; then
+			let counter-=1
+			sleep 5
+		else
+			return 0
+		fi
+	done
+	return 1
+}
+
+function check_new_master() {
+	counter=120
+	until [ $counter -le 0 ]; do
+		if [ $(cluster_master) == "${ORIG_MASTER}" ]; then
+			let counter-=1
+			sleep 5
+		else
+			return 0
+		fi
+	done
+	return 1
+}
+
+function check_master_count() {
+	counter=120
+	until [ $counter -le 0 ]; do
+		if [ $(cluster_status) == "red" ]; then
+			echo "Cluster went red during master heal"
+			return 1
+		fi
+		if [ $(master_count) != "${NUM_MASTER}" ]; then
+			let counter-=1
+			sleep 5
+		else
+			return 0
+		fi
+	done
+	return 1
+}
+
+function check_data_count() {
+	counter=120
+	until [ $counter -le 0 ]; do
+		if [ $(cluster_status) == "red" ]; then
+			echo "Cluster went red during data heal"
+			return 1
+		fi
+		if [ $(data_count) != "${NUM_DATA}" ]; then
+			let counter-=1
+			sleep 5
+		else
+			return 0
+		fi
+	done
+	return 1
+}
+
+# make sure we are green before starting
+
+if ! check_status; then
+	echo "Cluster didn't start green"
+	curl -v https://${url}/cluster-health
+	exit 1;
+fi
+
+# get our current master, and count
+ORIG_MASTER=$(cluster_master)
+NUM_MASTER=$(master_count)
+
+# kill it
+curl -ks -u"${K8S_USERNAME}:${K8S_PASSWORD}" ${K8S_APISERVER}/api/v1/namespaces/default/pods/${ORIG_MASTER} -XDELETE
+
+# wait for it to die, i.e. we should see 1 less master
+ORIG_NUM_MASTER=${NUM_MASTER}
+NUM_MASTER=$((${NUM_MASTER}-1))
+if ! check_master_count; then
+	echo "Failed to simulate master node failure"
+	curl -v https://${url}/cluster-health
+	curl -v https://${url}/cluster-state
+	exit 1;
+fi
+NUM_MASTER=${ORIG_NUM_MASTER}
+
+# ensure we pick a new master and stay green
+if ! check_new_master; then
+  echo "Failed to elect new master"
+  exit 1
+fi
+
+if ! check_status; then
+	echo "Cluster didn't stay green during master election"
+	curl -v https://${url}/cluster-health
+	exit 1;
+fi
+
+if ! check_master_count; then
+  echo "Failed to heal master"
+  exit 1
+fi
+
+# remember how many data nodes we should have
+NUM_DATA=$(data_count)
+
+# get a random data node
+DATA_NODE=$(curl -s "https://${url}/cluster-nodes" | jq -r '.nodes | map(select(.settings.node.data=="true")) | .[].name' | shuf | head -1)
+
+# kill it
+curl -ks -u"${K8S_USERNAME}:${K8S_PASSWORD}" ${K8S_APISERVER}/api/v1/namespaces/default/pods/${DATA_NODE} -XDELETE
+
+# wait for it to die, we should see one less data node
+ORIG_NUM_DATA=${NUM_DATA}
+NUM_DATA=$((${NUM_DATA}-1))
+if ! check_data_count; then
+	echo "Failed to simulate data node failure"
+	curl -v https://${url}/cluster-health
+	curl -v https://${url}/cluster-state
+	exit 1;
+fi
+NUM_DATA=${ORIG_NUM_DATA}
+
+# the data node should come back
+if ! check_data_count; then
+	echo "Cluster did not heal a lost data node"
+	curl -v https://${url}/cluster-health
+	curl -v https://${url}/cluster-state
+	exit 1;
+fi
+
+# the cluster should be green
+if ! check_status; then
+	echo "Cluster didn't go green after healing a data node"
+	curl -v https://${url}/cluster-health
+	exit 1;
+fi
---
+++ acceptance/elasticsearch56/main.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/cloudfoundry-community/go-cfenv"
+	"gopkg.in/olivere/elastic.v5"
+)
+
+func checkStatus(err error) {
+	if err != nil {
+		log.Fatalf("error: %s", err.Error())
+	}
+}
+
+func writeError(w http.ResponseWriter, err error) {
+	message, _ := json.Marshal(map[string]string{
+		"error": err.Error(),
+	})
+	w.Write(message)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusInternalServerError)
+}
+
+type Record struct {
+	Key   string
+	Value string
+}
+
+var client *elastic.Client
+
+func state(w http.ResponseWriter, r *http.Request) {
+	resp, err := client.ClusterState().Do(context.Background())
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+
+	jresp, err := json.Marshal(resp)
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jresp)
+}
+
+func nodes(w http.ResponseWriter, r *http.Request) {
+	resp, err := client.NodesInfo().Do(context.Background())
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+
+	jresp, err := json.Marshal(resp)
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jresp)
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	resp, err := client.ClusterHealth().Do(context.Background())
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+
+	jresp, err := json.Marshal(resp)
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jresp)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Set and check document
+	record := Record{Key: "key", Value: "value"}
+	_, err := client.Index().Index("test").Type("test").Id("1").BodyJson(record).Refresh("true").Do(context.Background())
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+
+	resp, err := client.Get().Index("test").Type("test").Id("1").Do(context.Background())
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+
+	if !resp.Found {
+		writeError(w, err)
+		return
+	}
+
+	result := Record{}
+	err = json.Unmarshal(*resp.Source, &result)
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+
+	if result.Value != "value" {
+		writeError(w, fmt.Errorf("incorrect value: %s", result.Value))
+		return
+	}
+
+	_, err = client.DeleteIndex("test").Do(context.Background())
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+}
+
+func main() {
+	// Get elasticsearch56-multinode credentials
+	env, _ := cfenv.Current()
+	services, _ := env.Services.WithLabel("elasticsearch56")
+	if len(services) != 1 {
+		log.Fatal("elasticsearch56 service not found")
+	}
+	creds := services[0].Credentials
+
+	// Create elasticsearch client
+	var err error
+	client, err = elastic.NewClient(
+		elastic.SetURL("http://"+creds["hostname"].(string)+":"+creds["port"].(string)),
+		elastic.SetBasicAuth(creds["username"].(string), creds["password"].(string)),
+		elastic.SetSniff(false),
+	)
+	checkStatus(err)
+
+	// Serve HTTP
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/cluster-health", health)
+	http.HandleFunc("/cluster-nodes", nodes)
+	http.HandleFunc("/cluster-state", state)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), nil))
+}
